33393ad03d65470f6c25e1d7097a5e23560f5ae8
Samotokhin Ilya <samotoxin@bkru>
2024-02-23 22:09:24 +0300

    added disabled btn

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const posts = []
+const TITLE_VALIDATION_LIMIT = 100
+const TEXT_VALIDATION_LIMIT = 300
 
 const titleInputNode = document.querySelector('.js-new-post__title')
 const textInputNode = document.querySelector('.js-new-post__text')
 const newPostBtnNode = document.querySelector('.js-new-post__btn')
 const postsNode = document.querySelector('.js-posts')
-const clearPostBtnNode = document.querySelector('.js-clear-post__btn')
-// Очищает только///
+const validationTitle = document.querySelector('.js-validationTitle')
+const validationText = document.querySelector('.js-validationText')
 
+//Отправка формы поста
 newPostBtnNode.addEventListener('click', function () {
 	const postFromUser = getPostFromUser()
 
@@ -14,7 +17,50 @@ newPostBtnNode.addEventListener('click', function () {
 
 	renderPosts()
 })
+//
 
+function addDisabledBtn() {
+	newPostBtnNode.setAttribute('disabled', '')
+	newPostBtnNode.style.backgroundColor = 'grey'
+	return
+}
+
+function removeDisabledBtn() {
+	newPostBtnNode.removeAttribute('disabled', '')
+	newPostBtnNode.style.backgroundColor = ''
+	return
+}
+
+//Длинна поста
+titleInputNode.addEventListener('input', function (event) {
+	const currentValue = event.target.value
+	validationTitle.innerText = `Длинна заголовка не должна превышать ${TITLE_VALIDATION_LIMIT} символов.`
+
+	if (currentValue.length > TITLE_VALIDATION_LIMIT) {
+		validationTitle.innerText = `Длинна заголовка не должна превышать ${TITLE_VALIDATION_LIMIT} символов.`
+		validationTitle.classList.remove('validationTitle__hidden')
+		addDisabledBtn()
+	} else {
+		validationTitle.classList.add('validationTitle__hidden')
+		removeDisabledBtn()
+	}
+})
+
+textInputNode.addEventListener('input', function (event) {
+	const currentValue = event.target.value
+
+	if (currentValue.length > TEXT_VALIDATION_LIMIT) {
+		validationText.innerText = `Длинна текста не должна превышать ${TEXT_VALIDATION_LIMIT} символов.`
+		validationText.classList.remove('validationText__hidden')
+		addDisabledBtn()
+	} else {
+		validationText.classList.add('validationText__hidden')
+		removeDisabledBtn()
+	}
+})
+//
+
+//Формирование формы поста
 function getPostFromUser() {
 	const title = titleInputNode.value
 	const text = textInputNode.value
@@ -49,13 +95,21 @@ function renderPosts() {
 	})
 	postsNode.innerHTML = postsHtml
 }
+//
 
-clearPostBtnNode.addEventListener('click', function () {
-	postsNode.innerHTML += date
-})
+//Отображение кнопок
+// titleInputNode.addEventListener('inner', function (event) {
+// 	const currentValue = event.target.value
 
-//Date
+// 	if (currentValue.length > TITLE_VALIDATION_LIMIT) {
+// 		newPostBtnNode.setAttribute('disabled', '')
+// 		return
+// 	}
+// })
 
+//
+
+//Даты
 function addLeadingZero(d) {
 	return d < 10 ? '0' + d : d
 }
@@ -79,5 +133,4 @@ function getUserTime(t = new Date()) {
 
 	return `${D}.${M}.${Y} ${h}:${m}`
 }
-
-// console.log(getUserTime())
+//
